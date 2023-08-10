110a861c750b04469ab1141a248927a7a78b5b64
Nikolaipe <nikolaipe@getacademy.no>
2023-08-10 12:20:23 +0200

    Basic controls

--- Sprite2Dtest.cs
+++ Sprite2Dtest.cs
@@ -8,11 +8,31 @@ public partial class Sprite2Dtest : Sprite2D
 	
 	public override void _Process(double delta)
 	{
-		Rotation -= _angularSpeed * (float)delta;
-		
-		var velocity = Vector2.Up.Rotated(Rotation) * _speed;
-		
-		Position -= velocity * (float)delta;
+		//Rotation -= _angularSpeed * (float)delta;
+
+		//var velocity = Vector2.Up.Rotated(Rotation) * _speed;
+
+
+		var direction = 0;
+		if (Input.IsActionPressed("ui_left"))
+		{
+			direction = -1;
+		}
+
+		if (Input.IsActionPressed("ui_right"))
+		{
+			direction = 1;
+		}
+
+		Rotation += _angularSpeed * direction * (float)delta;
+
+		var velocity = Vector2.Zero;
+		if (Input.IsActionPressed("ui_up"))
+		{
+			velocity = Vector2.Up.Rotated(Rotation) * _speed;
+		}
+
+		Position += velocity * (float)delta;
 	}
 	
 }
